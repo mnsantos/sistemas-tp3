150cc4bc70a8db60af3b18f30b98096314efa5cc
alumno <alumno@vmso.(none)>
2013-12-21 17:51:07 -0300

    asi es

--- tp3_code/code/ej2/reduce.js
+++ tp3_code/code/ej2/reduce.js
@@ -1,8 +1,8 @@
 function (key, values) {   
 	var reducedValue = {count : 0, comments : 0}
 	for (var idx = 0; idx < values.length; idx++){
-		count += values[idx].count;
-		comments += values[idx].comments;
+		reducedValue.count += values[idx].count;
+		reducedValue.comments += values[idx].comments;
 	}
 	return reducedValue;
 }
